af85d0524a169cd0f61d2f8ea2f18d004e9e1334
shjang7 <lucy.sh.jang@gmail.com>
2019-05-23 17:18:00 +0100

    Add stickeler

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require "./bubble_sort.rb"
 puts bubble_sort([4,3,78,2,0,2]).inspect
 
-r = bubble_sort_by(["hey","hello","hi","h"]) do |left,right|
+sorted_by_length = bubble_sort_by(["hey","hello","hi","h"]) do |left,right|
   left.length - right.length
-end
-puts r
+end.inspect
+puts sorted_by_length
